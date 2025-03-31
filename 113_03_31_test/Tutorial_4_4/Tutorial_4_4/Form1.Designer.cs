f43eee5bd7368f887d26f3c1bd7487242cf80f6a
a11223024 <a111223024@mail.shu.edu.tw>
2025-03-31 12:26:11 +0800

    113_03_31

---
+++ 113_03_31_test/Tutorial_4_4/Tutorial_4_4/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Tutorial_4_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label4 = new Label();
+            logListBox = new ListBox();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            label1.Location = new Point(82, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 20);
+            label1.TabIndex = 0;
+            label1.Text = "輸入行駛里程數";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            label2.Location = new Point(82, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(121, 20);
+            label2.TabIndex = 1;
+            label2.Text = "消耗油量公升數";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            label3.Location = new Point(82, 194);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 20);
+            label3.TabIndex = 2;
+            label3.Text = "您的平均油耗";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(298, 63);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(172, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(298, 127);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(172, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            button1.Location = new Point(82, 258);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 60);
+            button1.TabIndex = 6;
+            button1.Text = "計算";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            button2.Location = new Point(349, 258);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 60);
+            button2.TabIndex = 7;
+            button2.Text = "離開";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label4
+            // 
+            label4.BorderStyle = BorderStyle.Fixed3D;
+            label4.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            label4.Location = new Point(298, 203);
+            label4.Name = "label4";
+            label4.Size = new Size(185, 26);
+            label4.TabIndex = 8;
+            // 
+            // logListBox
+            // 
+            logListBox.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            logListBox.FormattingEnabled = true;
+            logListBox.ItemHeight = 24;
+            logListBox.Location = new Point(543, 63);
+            logListBox.Name = "logListBox";
+            logListBox.Size = new Size(199, 268);
+            logListBox.TabIndex = 9;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            button3.Location = new Point(543, 350);
+            button3.Name = "button3";
+            button3.Size = new Size(121, 60);
+            button3.TabIndex = 10;
+            button3.Text = "總平均油耗";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(logListBox);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Label label4;
+        private ListBox logListBox;
+        private Button button3;
+    }
+}
